d5ef81faff5f111a3247edd08d22c89fc61955d1
ScriptedSavant <gaurav.rathod.foryou@gmail.com>
2026-05-08 19:47:53 +0530

    Bishop Final Movement

--- Assets/Scripts/Pieces/Bishop.cs
+++ Assets/Scripts/Pieces/Bishop.cs
@@ -1,11 +1,42 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bishop : ChessPiece
 {
     public override List<Vector2Int> GetAvailableMoves()
     {
         List<Vector2Int> moves = new List<Vector2Int>();
+        Vector2Int[] direction = new[]
+        {
+            new Vector2Int(-1, 1),
+            new Vector2Int(1, 1),
+            new Vector2Int(-1, -1),
+            new Vector2Int(1, -1),
+        };
+        foreach (Vector2Int dir in direction)
+        {
+            for (int i = 1; i < 8; i++)
+            {
+                Vector2Int targetPos = new Vector2Int(gridPosition.x + dir.x * i, gridPosition.y + dir.y * i);
+                Tile t = Board.Instance.GetTileAtPosition(targetPos.x, targetPos.y);
+
+                if (t == null) break;
+                if (!t.isOccupied)
+                {
+                    moves.Add(targetPos);
+                }
+                else
+                {
+                    if (t.GetChessPiece().GetTeam != this.GetTeam)
+                    {
+                        moves.Add(targetPos);
+                    }
+
+                    break;
+                }
+            }
+        }
 
         return moves;
     }
