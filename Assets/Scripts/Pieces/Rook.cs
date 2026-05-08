e9bd5727e4a4b059a2b4a747a7fdfb11cc826a82
ScriptedSavant <gaurav.rathod.foryou@gmail.com>
2026-05-08 18:14:19 +0530

    New Rook Movement

--- Assets/Scripts/Pieces/Rook.cs
+++ Assets/Scripts/Pieces/Rook.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Rook : ChessPiece
@@ -6,6 +7,37 @@ public class Rook : ChessPiece
     public override List<Vector2Int> GetAvailableMoves()
     {
         List<Vector2Int> moves = new List<Vector2Int>();
+        Vector2Int[] direction = new[]
+        {
+            new Vector2Int(-1, 0),
+            new Vector2Int(1, 0),
+            new Vector2Int(0, 1),
+            new Vector2Int(0, -1),
+        };
+        foreach (Vector2Int dir in direction)
+        {
+            for (int i = 1; i < 8; i++)
+            {
+                Vector2Int targetPos = new Vector2Int(gridPosition.x + dir.x * i, gridPosition.y + dir.y * i);
+                Tile t = Board.Instance.GetTileAtPosition(targetPos.x, targetPos.y);
+                if (t == null) break;
+
+                if (!t.isOccupied)
+                {
+                    moves.Add(targetPos);
+                }
+                else
+                {
+                    if (t.GetChessPiece().GetTeam != this.GetTeam)
+                    {
+                        moves.Add(targetPos);
+                    }
+
+                    break;
+                }
+            }
+        }
+
         return moves;
     }
 }
