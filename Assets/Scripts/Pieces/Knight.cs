98e3208698d57fe926bd3276cf397bd8a8de982a
ScriptedSavant <gaurav.rathod.foryou@gmail.com>
2026-05-08 19:25:25 +0530

    Knight Final Movement

--- Assets/Scripts/Pieces/Knight.cs
+++ Assets/Scripts/Pieces/Knight.cs
@@ -6,6 +6,36 @@ public class Knight : ChessPiece
     public override List<Vector2Int> GetAvailableMoves()
     {
         List<Vector2Int> moves = new List<Vector2Int>();
+        Vector2Int[] direction = new[]
+        {
+            new Vector2Int(-1, 2),
+            new Vector2Int(1, 2),
+            new Vector2Int(-2, 1),
+            new Vector2Int(2, 1),
+            new Vector2Int(-2, -1),
+            new Vector2Int(2, -1),
+            new Vector2Int(-1, -2),
+            new Vector2Int(1, -2),
+        };
+        foreach (Vector2Int dir in direction)
+        {
+            Vector2Int targetPos = new Vector2Int(gridPosition.x + dir.x, gridPosition.y + dir.y);
+            Tile t = Board.Instance.GetTileAtPosition(targetPos.x, targetPos.y);
+
+            if (t == null) continue;
+
+            if (!t.isOccupied)
+            {
+                moves.Add(targetPos);
+            }
+            else
+            {
+                if (t.GetChessPiece().GetTeam != this.GetTeam)
+                {
+                    moves.Add(targetPos);
+                }
+            }
+        }
 
         return moves;
     }
